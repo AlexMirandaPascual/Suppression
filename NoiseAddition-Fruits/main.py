c00bbdd7605fb0a0267ef2359ad209e06d54ae70
Àlex Miranda <alex.pascual@kit.edu>
2024-12-17 17:09:26 +0100

    Corrected document

--- NoiseAddition-Fruits/main.py
+++ NoiseAddition-Fruits/main.py
@@ -1,22 +1,9 @@
-from function_dp import *
-from generate_distances import *
+from principal_function import *
 
-# filedistanceL2()
-# generate_files_m_M(m=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
-#                         path_average_distances="Appledistances.csv",
-#                         path_of_file="Files m and M\\Apple\\")
-# generate_supression_file(path_m_M="Files m and M\\Apple\\", original_dataset_path="fruits.csv", fruit_name="Apple", numberofrepeat=100)
-# agregateLaplaceandGaussian()
-# calculateAverageofelement()
-# combining_averages()
-# agregatefileoriginalPrima()
-# calculateAverageofelement(file="File_graphic\\HighestEducationCompleted_onlysupression.csv", File_name= "File_graphic\\AverageHighestEducationCompleted.csv")
-# combining_averages(path_average_supression = "File_graphic\\AverageHighestEducationCompleted.csv",
-#         path_average_prima = "File_graphic\\originalHighestEducationCompleted prima.csv",
-#         file= "File_graphic\\CombiningHighestEducationCompleted.csv")
+generateFileandGraphApple()
 
-# df=pd.read_csv("fruits.csv")
-# df_elements=df[df["Fruit Name"]=="Melon"]
-# print(df_elements.describe())
-# ele=prob_dataset(probabilitic="Files m and M\\Apple\\file m=0.1 M=0.1.csv", dataset="fruits.csv", fruit_name="Apple")
-# print(ele)
+generateFileandGraphMelon()
+
+generateFileandGraphPear()
+
+generateFileandGraphStrawberry()
