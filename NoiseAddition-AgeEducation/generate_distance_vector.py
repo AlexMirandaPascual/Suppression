981315ce4ac3920f93910b5db2f46d4bff6adfec
AlexMirandaPascual <alex.pascual@kit.edu>
2024-12-28 18:58:37 +0100

    Update and rename generate_distances.py to generate_distance_vector.py

--- NoiseAddition-AgeEducation/generate_distance_vector.py
+++ NoiseAddition-AgeEducation/generate_distance_vector.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def filedistanceL2(filename="irishn_train.csv", column="Age"):
+def generate_distances(filename="irishn_train.csv", column="Age"):
     # Normalize Age vector
     df=pd.read_csv(filename)
     Element=df[column]
