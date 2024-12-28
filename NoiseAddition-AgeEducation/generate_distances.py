b845d931104acf3b5a8bdf08f2d5122064d914a0
AlexMirandaPascual <alex.pascual@kit.edu>
2024-12-28 18:34:02 +0100

    Update generate_distances.py

--- NoiseAddition-AgeEducation/generate_distances.py
+++ NoiseAddition-AgeEducation/generate_distances.py
@@ -6,19 +6,19 @@ def filedistanceL2(filename="irishn_train.csv", column="Age"):
 
     df=pd.read_csv(filename)
     Element=df[column]
-    max_valor_Element=Element.max()
-    Element_norm=Element/max_valor_Element
-    promdistancias=[]
+    max_value_Element=Element.max()
+    Element_norm=Element/max_value_Element
+    outlier_score_list=[]
 
-    countable_elements=len(Element_norm)
-    for i in range(countable_elements): 
-        suma_total=0
-        for j in range(countable_elements): 
+    length_Element=len(Element_norm)
+    for i in range(length_Element): 
+        total_sum=0
+        for j in range(length_Element): 
             dist=np.linalg.norm([Element_norm[i], Element_norm[j]])
-            suma_total=suma_total + dist
-        promedio=suma_total/(countable_elements)
-        promdistancias.append(promedio)
-    dataframenumpy=np.array(promdistancias)
+            total_sum=total_sum + dist
+        outlier_score=total_sum/(length_Element)
+        outlier_score_list.append(outlier_score)
+    dataframenumpy=np.array(outlier_score_list)
     file=column + "distances.csv"
     # dataframe.to_csv(path="algo.csv", header=["Average distances"], index=None)
     np.savetxt(file, dataframenumpy)
