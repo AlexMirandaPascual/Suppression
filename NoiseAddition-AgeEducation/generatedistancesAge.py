4981e1ab73f738d2bafc50f6a32f72399e489c7f
Àlex Miranda <alex.pascual@kit.edu>
2024-12-29 13:46:10 +0100

    New generated distances

---
+++ NoiseAddition-AgeEducation/generatedistancesAge.py
@@ -0,0 +1,4 @@
+from principal_function import *
+
+generate_distances()
+
---
+++ NoiseAddition-AgeEducation/generatedistancesEducation.py
@@ -0,0 +1,4 @@
+from principal_function import *
+
+generate_distances(filename="irishn_train.csv", column="HighestEducationCompleted")
+
